6dd57b4955689840b1a14fdb9102f64232760c45
Christian G. Warden <cwarden@xerus.org>
2011-12-14 14:50:10 -0800

    Create ThinkUp config file, database tables, and user

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,24 @@
-class thinkup($webserver = 'apache', $port = 80) {
+Class['thinkup::config'] -> Class['thinkup::server']
+Class['thinkup::config'] -> Class['thinkup::database']
+Class['thinkup::config'] -> Class['thinkup::server::database_tables']
 
+class thinkup::config(
+  $database_host,
+  $database_user,
+  $database_password,
+  $database,
+  $timezone = 'America/Los_Angeles',
+  $password_salt
+) { }
+
+class thinkup::server($webserver = 'apache', $port = 80) {
+  $database_host = $thinkup::config::database_host
+  $database_user = $thinkup::config::database_user
+  $database_password = $thinkup::config::database_password
+  $database = $thinkup::config::database
+  $timezone = $thinkup::config::timezone
+
+  include mysql
   file { "thinkup_1.0_all.deb":
     path   => "/var/cache/apt/archives/thinkup_1.0_all.deb",
     source => "puppet:///modules/thinkup/thinkup_1.0_all.deb",
@@ -48,10 +67,35 @@ class thinkup($webserver = 'apache', $port = 80) {
   }
 
   file { "/var/www/thinkup/_lib/view/compiled_view/":
-    ensure => directory,
-    mode   => 700,
-    owner  => "www-data",
-    group  => "www-data",
+    ensure  => directory,
+    mode    => 700,
+    owner   => "www-data",
+    group   => "www-data",
+    require => Package['thinkup']
+  }
+
+  file { "/var/www/thinkup/logs/":
+    ensure  => directory,
+    mode    => 700,
+    recurse => true,
+    owner   => "www-data",
+    group   => "www-data",
+    require => Package['thinkup']
+  }
+
+  file { "/var/www/thinkup/tmp/":
+    ensure  => directory,
+    mode    => 700,
+    recurse => true,
+    purge   => true,
+    owner   => "root",
+    group   => "root",
+    require => Package['thinkup']
+  }
+
+  file { "/var/www/thinkup/config.inc.php":
+    content => template('thinkup/config.inc.php'),
+    require => Package['thinkup']
   }
 }
 
@@ -67,5 +111,86 @@ class thinkup::proxy($listen_host, $listen_port = 80, $destination_host, $destin
     listen_port => $listen_port,
     ensure   => present,
     proxy  => 'http://thinkup',
+  }
+}
+
+class thinkup::server::database_tables {
+  $create_sql = '/var/www/thinkup/tmp/build-db_mysql.sql'
+  file { $create_sql:
+    ensure => present,
+    source => '/var/www/thinkup/install/sql/build-db_mysql.sql'
+  }
+
+  Exec {
+    path    => [ '/usr/local/bin', '/usr/bin', '/bin' ],
+    require => tagged('mysqlserver') ? { true => Class['thinkup::database'], default => undef }
+  }
+
+  $mysql = "mysql -h${thinkup::config::database_host} -u${thinkup::config::database_user} -p${thinkup::config::database_password} ${thinkup::config::database}"
+  exec {
+    "import thinkup":
+      command     => "${mysql} < ${create_sql}",
+      #unless     => "echo 'SHOW TABLES' | ${mysql} | grep -q instances",
+      refreshonly => true,
+      subscribe   => File[$create_sql];
+  }
+}
+
+class thinkup::database($type = 'mysql', $admin_password) {
+  case $type {
+    'mysql': {
+      if !defined(Class['mysql::server']) {
+        class { 'mysql::server':
+          config_hash => {
+            'root_password' => $admin_password,
+            'bind_address'  => tagged('thinkup::server') ?  {
+              true  => '127.0.0.1',
+              false => '0.0.0.0'
+            }
+          }
+        }
+      }
+
+      mysql::db { $thinkup::config::database:
+        user     => $thinkup::config::database_user,
+        password => $thinkup::config::database_password,
+        # TODO: restrict hosts
+        host     => '%',
+        grant    => ['all'],
+      }
+
+    }
+    default: { fail("Unrecognized database type") }
+  }
+}
+
+define thinkup::user($fullname = $title, $email, $password, $admin = 0) {
+  Class['thinkup::server::database_tables'] -> Thinkup::User[$title]
+
+  Exec {
+    path => [ '/usr/local/bin', '/usr/bin', '/bin' ]
+  }
+
+  # creates a file containing sql to populate owner record, then execs mysql client
+  # use generally-static salt so the template doesn't return different content on each run
+  $sql_tmpl = "<% require 'digest';
+    salt = Digest::SHA256.hexdigest('${thinkup::config::password_salt}' + '${email}');
+    pass = Digest::SHA256.hexdigest('${password}' + salt) %>
+    INSERT INTO tu_owners (`full_name`, `email`, `pwd`, `pwd_salt`, `is_activated`, `is_admin`)
+    VALUES ('${fullname}', '${email}', '<%= pass -%>', '<%= salt -%>', 1, ${admin})
+    ON DUPLICATE KEY UPDATE full_name = '${fullname}', pwd = '<%= pass -%>', pwd_salt = '<%= salt -%>', is_activated = 1, is_admin = ${admin};"
+  $sql = inline_template($sql_tmpl)
+  $sql_file = md5($sql)
+  $sql_path = "/var/www/thinkup/tmp/${sql_file}"
+
+  file { "${sql_path}":
+    owner   => 'puppet',
+    mode    => '600',
+    content => $sql,
+  }
+  exec { "create owner $title":
+    command => "mysql -h ${thinkup::config::database_host} -u${thinkup::config::database_user} -p${thinkup::config::database_password} ${thinkup::config::database} < ${sql_path}",
+    refreshonly => true,
+    subscribe => File[$sql_path]
   }
 }
---
+++ templates/config.inc.php
@@ -0,0 +1,64 @@
+<?php
+/************************************************/
+/***  APPLICATION CONFIG                      ***/
+/************************************************/
+
+// Application title
+$THINKUP_CFG['app_title']                 = 'ThinkUp';
+
+// Public path of thinkup's /webapp/ folder on your web server.
+// For example, if the /webapp/ folder is located at http://yourdomain/thinkup/, set to '/thinkup/'.
+$THINKUP_CFG['site_root_path']            = '/';
+
+// Full server path to /thinkup/ folder.
+$THINKUP_CFG['source_root_path']          = realpath(dirname(__FILE__)) . '/';
+
+// Your timezone
+$THINKUP_CFG['timezone']                  = '<%= timezone -%>';
+
+// Toggle Smarty caching. true: Smarty caching on, false: Smarty caching off
+$THINKUP_CFG['cache_pages']               = true;
+
+// The crawler, when triggered by requests to the RSS feed, will only launch if it's been
+// 20 minutes or more since the last crawl.
+$THINKUP_CFG['rss_crawler_refresh_rate']  = 20;
+
+/************************************************/
+/***  DATABASE CONFIG                         ***/
+/************************************************/
+
+$THINKUP_CFG['db_host']                   = '<%= database_host -%>'; //On a shared host? Try mysql.yourdomain.com, or see your web host's documentation.
+$THINKUP_CFG['db_type']                   = 'mysql';
+$THINKUP_CFG['db_user']                   = '<%= database_user -%>';
+$THINKUP_CFG['db_password']               = '<%= database_password -%>';
+$THINKUP_CFG['db_name']                   = '<%= database -%>';
+$THINKUP_CFG['db_socket']                 = '';
+$THINKUP_CFG['db_port']                   = '';
+$THINKUP_CFG['table_prefix']              = 'tu_';
+
+/************************************************/
+/***  DEVELOPER CONFIG                        ***/
+/************************************************/
+
+// Full server path to crawler.log.
+$THINKUP_CFG['log_location']              = dirname(__FILE__) . '/../logs/crawler.log';
+
+// Verbosity of log. 0 is everything, 1 is user messages, 2 is errors only
+$THINKUP_CFG['log_verbosity']             = 0;
+
+// Full server path to stream processor log.
+$THINKUP_CFG['stream_log_location']       = dirname(__FILE__) . '/../logs/stream.log';
+
+// Full server path to sql.log. To not log queries, set to null.
+$THINKUP_CFG['sql_log_location']          = dirname(__FILE__) . '/../logs/sql.log';
+
+// How many seconds does a query take before it gets logged as a slow query?
+$THINKUP_CFG['slow_query_log_threshold']  = 2.0;
+
+$THINKUP_CFG['debug']                     = false;
+
+$THINKUP_CFG['enable_profiler']           = false;
+
+// Set this to true if you want your PDO object's database connection's charset to be explicitly set to utf8.
+// If false (or unset), the database connection's charset will not be explicitly set.
+$THINKUP_CFG['set_pdo_charset']           = false;
