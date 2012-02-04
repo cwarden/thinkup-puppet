2ab9ee9dd7bdba92eca98d5129c7ff6dac06a3e5
Christian G. Warden <cwarden@xerus.org>
2012-02-03 17:03:43 -0800

    Fix creation of ThinkUp schema upon failure
    - Don't add tu_posts.last_updated column until schema has been imported
    
    - Delete sql creation script if creation fails

--- manifests/init.pp
+++ manifests/init.pp
@@ -141,12 +141,15 @@ class thinkup::server::database_tables {
 
   Exec {
     path    => [ '/usr/local/bin', '/usr/bin', '/bin' ],
-    require => tagged('mysqlserver') ? { true => Class['thinkup::database'], default => undef }
+    require => tagged('mysqlserver') ? {
+      true => Class['thinkup::database'],
+      default => undef
+    }
   }
 
   $mysql = "mysql -h${thinkup::config::database_host} -u${thinkup::config::database_user} -p${thinkup::config::database_password} ${thinkup::config::database}"
   exec { "import thinkup":
-      command     => "${mysql} < ${create_sql}",
+      command     => "${mysql} < ${create_sql} || rm ${create_sql}",
       #unless     => "echo 'SHOW TABLES' | ${mysql} | grep -q instances",
       refreshonly => true,
       subscribe   => File[$create_sql],
@@ -156,6 +159,7 @@ class thinkup::server::database_tables {
   exec { "add tu_posts.last_updated":
       command => "echo 'ALTER TABLE tu_posts ADD last_updated TIMESTAMP; UPDATE tu_posts SET last_updated = NOW();' | ${mysql}",
       unless  => "echo 'SHOW COLUMNS FROM tu_posts LIKE \"last_updated\";' | ${mysql} | grep -q last_updated",
+      require => Exec['import thinkup']
   }
 }
 
